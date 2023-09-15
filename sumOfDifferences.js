038c8b5bee56c5936f78b8cf0218270d630fc7b8
Ren Chen <renchentheprogrammer@gmail.com>
2023-09-14 21:01:59 -0700

    added coding challenge 14 Sept

---
+++ sumOfDifferences.js
@@ -0,0 +1,11 @@
+// sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0
+
+sumOfDifferences = arr => arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0
