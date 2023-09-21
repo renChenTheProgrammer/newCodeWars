da14ef622501eaaf0d00001655ee6fdefd671e6f
Ren Chen <renchentheprogrammer@gmail.com>
2023-09-21 17:23:53 -0500

    coding challenge 21 September

---
+++ maxAndMinOfArray.js
@@ -0,0 +1,4 @@
+//make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
