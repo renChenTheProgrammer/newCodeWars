d59cde8b93391cbec2dea49f89388323a54d3211
Ren Chen <renchentheprogrammer@gmail.com>
2023-09-22 21:37:11 -0500

    coding challenge 22 September

---
+++ gradeAverage.js
@@ -0,0 +1,12 @@
+//Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+
+
+function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
