2ea57e88079a34e15fa48f30d8cb113a4677d5a1
Ren Chen <renchentheprogrammer@gmail.com>
2023-10-04 20:44:21 -0500

    added coding challenge 4 October

---
+++ willYouMakeIt.js
@@ -0,0 +1,9 @@
+// fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+
+
+zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuelLeft ? true : false;
